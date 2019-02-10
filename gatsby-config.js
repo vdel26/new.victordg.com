000b0dbe353ad2edd4e384362e4661a2686792c2
Victor Delgado <vdel26@gmail.com>
2019-02-10 16:56:36 +0100

    fix error with icon file

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,6 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#000',
         display: 'minimal-ui',
-        icon: 'src/images/favicon.ico', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
